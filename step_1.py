741f2dd24b768230b002b07298a20b4a17e8c758
AnnaR20 <avrusinova@mail.ru>
2020-06-11 15:54:32 +0300

    Initial commit

---
+++ step_1.py
@@ -0,0 +1,30 @@
+import tkinter
+#1
+
+# perem_1 = 10
+# perem_2 = 0.12
+# str_1 = 'snake'
+#
+# print(perem_1)
+# print(perem_1+perem_2)
+# print(str_1)
+#
+# name = input('Your name: ')
+# age = int(input('Your age: '))
+# print('Hello', name, ', born in', 2020 - age)
+
+#2
+
+# import time
+# time = time.gmtime(int(input('Введите время в сек: ')))
+# print(f'0{time.tm_hour}:{time.tm_min}:{time.tm_sec}')
+
+#3
+# n = int(input('Введите число: '))
+# print(n + (n*10 + n) + (n*100 + n*10 + n))
+
+#4
+#later
+
+
+
